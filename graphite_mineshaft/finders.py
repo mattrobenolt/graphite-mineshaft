a63a62ce60b8dfd61ebfbaffcca6e48675613469
Matt Robenolt <matt@ydekproductions.com>
2014-08-23 07:59:00 +0000

    Add fetch_multi support

--- graphite_mineshaft/finders.py
+++ graphite_mineshaft/finders.py
@@ -29,12 +29,26 @@ class MineshaftFinder(object):
             if node.leaf:
                 yield MineshaftLeafNode(node.key, MineshaftReader(node.key, self))
             else:
-                yield BranchNode(node.key)
+                yield MineshaftBranchNode(node.key)
 
     def fetch_multi(self, nodes, start_time, end_time):
-        targets = [node.path for node in nodes]
-        data = self.driver.metrics(targets, start_time, end_time)
+        step = end_time - start_time
+        series = {}
+        paths = [node.path for node in nodes]
+        data = self.driver.metrics(paths, start_time, end_time)
+        if data:
+            # Collect all the series into their own dict
+            series = {}
+            for key, value in data.iteritems():
+                series[key] = value['series']
 
+            # Extract the time info out of the first key
+            src = data.keys()[0]
+            start_time = data[src]['from']
+            end_time = data[src]['to']
+            step = data[src]['step']
+
+        return (start_time, end_time, step), series
 
 
 class MineshaftReader(object):
@@ -45,7 +59,6 @@ class MineshaftReader(object):
         self.finder = finder
 
     def fetch(self, start_time, end_time):
-        print self.path, start_time, end_time
         data = self.finder.driver.metrics(self.path, start_time, end_time).get(self.path, {})
         if not data:
             return (start_time, end_time, end_time - start_time), []
