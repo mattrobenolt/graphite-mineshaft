cf763c4e4081d8805b8aa69ded69367840e66a24
Matt Robenolt <matt@ydekproductions.com>
2014-12-27 12:58:45 -0500

    0.0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.rst') as f:
 
 setup(
     name='graphite-mineshaft',
-    version='0.0.0',
+    version='0.0.1',
     author='Matt Robenolt',
     author_email='matt@ydekproductions.com',
     url='https://github.com/mattrobenolt/graphite-mineshaft',
